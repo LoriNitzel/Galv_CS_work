5006c4056b3588857f3f1d85dac9aa2f3cd56c3a
Lori Nitzel <lorinitzel@mac.com>
2016-04-04 11:50:11 -0600

    CS problems

---
+++ warmup4-1-16.js
@@ -0,0 +1,54 @@
+//factorial function that calls itself and breaks it down to simplest format - no for loop or while loop but will have to call it again
+
+
+// RECURSIVE FACTORIAL
+
+// function factorial(num){
+//  if (num <= 0) {
+//   return 1; 
+//  } else {
+//   return (num * factorial(num - 1));
+//  }
+// }
+
+// console.log(factorial(10));
+
+
+//INTERATIVE FACTORIAL 
+
+
+
+
+
+
+
+// RECURSIVE FIBONACCI
+
+// function fibonacci(num){
+//   if (num <= 1){
+//     return num;
+//   } else {
+//     return fibonacci(num-1) + fibonacci(num-2);
+//   }
+// }
+
+// console.log(fibonacci(10));
+
+
+//ITERATIVE FIBONACCI 
+
+function fibonacci(num){
+  var prev = 0;
+  var curr = 1;
+  var hold = 0;
+  if (n <= 1) {
+    return n; 
+  } else {
+    
+  }
+}
+
+
+
+
+
---
+++ warmup4-4-16.js
@@ -0,0 +1,63 @@
+// Create your own function called tripleLoop, that runs 3 embedded for loops.
+
+// For each of the preceding functions, get the runtime starting at size 1000 to size 50000, at intervals of 500. Make a spreadsheet with the results, and create a graph that maps input size to runtime.
+
+
+
+function bigOH(num){
+  for(var i = 0; i < num; i++){
+    
+  }
+}
+
+
+
+// function loop(size){
+//   var sum = 0;
+//   for(var i = 0; i < size; i++){
+//     sum++;
+//   }
+// };
+
+// var start = process.hrtime();
+// function doubleLoop(size){
+//   var sum = 0;
+//   for(var i = 0; i < size; i++){
+//     for(var j = 0; j < size; j++){
+//         sum+=500;
+//     }
+//   }
+// }
+// console.log(loop(1000));
+// var end = process.hrtime(start);
+// console.log(end);
+
+
+
+
+
+
+
+// function tripleLoop(size){
+//   var sum = 0;
+//   for (var i = 0; i < 1000; i++){
+//     for (var j = 0; j < size; j++){
+//       for (var k = 0; k < size; k++){
+//         sum++
+//       }
+//     }
+//   }
+// }
+
+// var time = process.hrtime();
+// // [ 1800216, 25 ]
+
+// setTimeout(() => {
+//   var diff = process.hrtime(time);
+//   // [ 1, 552 ]
+
+//   console.log('benchmark took %d nanoseconds', diff[0] * 1e9 + diff[1]);
+//   // benchmark took 1000000527 nanoseconds
+// }, 2000);
+
+
