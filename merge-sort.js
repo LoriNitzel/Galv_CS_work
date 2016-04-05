0d288b85655f6743906f983de832cba6070b59f3
Lori Nitzel <lorinitzel@mac.com>
2016-04-05 11:48:30 -0600

    updated merge-sort

--- merge-sort.js
+++ merge-sort.js
@@ -12,7 +12,7 @@ function merge(array1, array2){
       mergedArray.push(array2[array2_index]);
       array2_index++; 
 
-    } else if (array1[i] === array2[i]){
+    } else if (array1[array1_index] === array2[array2_index]){
       mergedArray.push(array1[array1_index]);
       mergedArray.push(array2[array2_index]);
       array1_index++; 
@@ -35,7 +35,33 @@ function mergeSort(list){
     return list; 
   }
 
+  var middle = Math.floor(list.length / 2);
+  var left = list.slice(0,middle);
+  var right = list.slice(middle);
+
   return merge(mergeSort(left), mergeSort(right)); 
 }
 
-console.log(merge([1,5,7,10,11],[2,8,9]))
+// console.log(merge([1,5,7,10,11],[2,8,9]))
+
+for (var i = 0; i < 10; i++){
+  console.log(testPerformance(mergeSort,getRandomList(298983*i)));
+}
+
+
+
+function getRandomList(length){
+  var list = [];
+  for(var i = 0; i < length; i++){
+    list.push(Math.floor(Math.random() * length*2)+1);
+  }
+  return list;
+}
+
+
+function testPerformance(callback, args) {
+  var t0 = process.hrtime();
+  callback(args);
+  var diff = process.hrtime(t0);
+  return(diff[0] * 1e9 + diff[1])
+}
